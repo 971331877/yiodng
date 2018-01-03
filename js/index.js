ef3de48eb52ad3e741885b8add0aa6e63a4bec60
971331877 <you@example.com>
2018-01-04 01:16:30 +0800

    yidong

--- js/index.js
+++ js/index.js
@@ -67,3 +67,91 @@
             })
     })
 }
+
+// // 返回顶部
+{
+    let totop=document.querySelector("#menu ul li.back span");
+    totop.onclick=function () {
+        //     document.documentElement.scrollTop=0;
+        let st=document.documentElement.scrollTop;
+        let speend=st*30/500;
+        let t= setInterval(function () {
+            st-=speend;
+            if(st<=0){
+                st=0;
+                clearInterval(t);
+            }
+            document.documentElement.scrollTop=st;
+
+        },30)
+    }
+}
+//
+{
+    let topbanr=document.querySelector(".dh");
+    //console.log(topbanr);
+    let letbars=document.querySelector("#menu");
+
+    let floors=document.querySelectorAll(".xyh");
+    let flag=true;
+    window.onscroll=function () {
+        if(flag){
+            let st= document.documentElement.scrollTop;
+            if(st>450){
+                topbanr.style.display="block";
+                // topbanr.style.top="0"
+            }else{
+                topbanr.style.display="none"
+                // topbanr.style.top="-40"
+            }
+            if(st>700){
+                // console.log(st)
+                console.log(1);
+                letbars.style.display="block";
+            }else{
+                letbars.style.display="none";
+            }
+
+            floors.forEach(function (ele,index) {
+                if(st>ele.offsetTop-160){
+                    for(let i=0;i<btns.length;i++){
+                        btns[i].classList.remove("active");
+                    }
+                    btns[index].classList.add("active");
+                }
+
+            })
+        }
+    }
+
+
+
+
+    //  跳转楼层
+
+
+    let btns=document.querySelectorAll("#menu .btn");
+ //   let floors=document.querySelectorAll(".xyh");
+
+    console.log(btns)
+    btns.forEach(function (ele,index) {
+        ele.onclick=function () {
+            flag=false;
+            let ot=floors[index].offsetTop;
+            let now=document.documentElement.scrollTop;
+            let speed=(ot-now)*30/300;
+            let time=0;
+            let t=setInterval(function () {
+                now+=speed;
+                time+=30;
+                if(time==300){
+                    clearInterval(t);
+                    now=ot;
+                    flag=true;
+                }
+                document.documentElement.scrollTop=now;
+            },30)
+            // document.documentElement.scrollTop=ot;
+        }
+    });
+}
